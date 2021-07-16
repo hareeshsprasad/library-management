a520a2a6dd414669c2f5aa14f0e3eef2e7276563
hareeshsprasad <hareeshsprasad@gmail.com>
2021-07-16 10:35:45 +0530

    session management

--- librarian/approve.php
+++ librarian/approve.php
@@ -1,4 +1,13 @@
 <?php
+session_start();
+if(!isset($_SESSION["librarian"])){
+	?>
+	<script type="text/javascript">
+	   window.location="login.php";
+   </script>
+	<?php
+	   
+   }
 include "connection.php";
 $id=$_GET["id"];
 mysqli_query($link,"update student_registration set status='yes' where id=$id");
